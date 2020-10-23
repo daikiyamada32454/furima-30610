04529f6a09b459ea6a99a181c646f3c779d00af6
daikiyamada32454 <waltz32454@gmail.com>
2020-10-23 17:47:48 +0900

    テストコードの記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Internet.username}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length:6)}
+    password_confirmation {password}
+    family_name           {Gimei.last.kanji}   
+    family_name_kana      {Gimei.last.katakana}
+    first_name            {Gimei.first.kanji}
+    first_name_kana       {Gimei.first.katakana}
+    birthday              {"1997-11-14"}
+  end
+end
