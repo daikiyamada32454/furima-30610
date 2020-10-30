1cc19b1354af6dcd3ea1399a29ff330ef86ca8b5
daikiyamada32454 <waltz32454@gmail.com>
2020-10-30 17:49:13 +0900

    削除機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
    def index
     @item = Item.order("id")
    end 
@@ -35,8 +35,10 @@ class ItemsController < ApplicationController
    end
 
    def destroy
-     @item = Item.find(params[:id])
-     @item.destroy
+    current_user.id == @item.user_id
+     if @item.destroy
+      redirect_to root_path
+     end
    end
   private
   def item_params
