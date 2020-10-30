31b7b9e325da2760ae58d3c4f3d2db0655336ca7
daikiyamada32454 <waltz32454@gmail.com>
2020-10-30 16:04:03 +0900

    商品編集機能　修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
    def index
     @item = Item.order("id")
    end 
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
    end 
 
    def update
-     @item = Item.find(params[:id])
      if  @item.update(item_params)
       redirect_to item_path
     else
