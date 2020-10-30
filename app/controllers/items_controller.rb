9424f3bfdcd0fbb55a060da51d05e0f01cb945af
daikiyamada32454 <waltz32454@gmail.com>
2020-10-30 11:06:24 +0900

    商品詳細機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,9 +18,11 @@ class ItemsController < ApplicationController
    end
 
    def show
+    @item = Item.find(params[:id])
    end
 
    def edit
+    @item = Item.find(params[:id])
    end
 
    def destroy
