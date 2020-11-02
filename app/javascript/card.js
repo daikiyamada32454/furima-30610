af6b409eb4e2b823011f031924fd3013dc924a7b
daikiyamada32454 <waltz32454@gmail.com>
2020-11-02 18:49:21 +0900

    カード情報をPAY.JP側に送り、トークンが返ってくる

---
+++ app/javascript/card.js
@@ -0,0 +1,26 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_0380bd3d42495187b615bef7");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
